9e83840b1100811016c74ef7f2dfe8746d67f335
tomchaccom <dreamkms2014@gmail.com>
2025-03-11 21:14:39 +0900

    chat

---
+++ src/test/java/com/web2/web2/UserLogin.java
@@ -0,0 +1,33 @@
+package com.web2.web2;
+
+import com.web2.user.UserService;
+import com.web2.user.dto.LoginUser;
+import jakarta.servlet.http.HttpSession;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserLogin {
+
+/*
+    @Autowired
+    public UserService userService;
+
+
+    @Test
+    public void 로그인(HttpSession session){
+
+        LoginUser loginUser = new LoginUser("11@naver.com" , "000000000");
+        userService.login(loginUser);
+
+        System.out.println(session.getAttribute("SESSION_ID"));
+
+
+    }
+*/
+
+
+
+
+}
